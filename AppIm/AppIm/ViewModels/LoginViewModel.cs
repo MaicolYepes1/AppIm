56af9ac8e1e53f17f3c3ebe5fdafb81cc13a33f7
Maicol <MaicolYepes1@Gmail.com>
2018-10-16 16:49:11 -0500

    Hola

---
+++ AppIm/AppIm/ViewModels/LoginViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace AppIm.ViewModels
+{
+    class LoginViewModel
+    {
+        #region Properties
+        public string Usuario
+        {
+            get;
+            set;
+        }
+        public string Contraseña
+        {
+            get;
+            set;
+        }
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+        #endregion
+
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get;
+            set;
+        }
+        #endregion
+    }
+}
