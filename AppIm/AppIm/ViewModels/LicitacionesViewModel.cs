e50b7e6234adf184442dd2b10b480522de7e045f
MaicolYepes1 <44202549+MaicolYepes1@users.noreply.github.com>
2018-10-25 09:01:37 -0500

    Master Page

---
+++ AppIm/AppIm/ViewModels/LicitacionesViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AppIm.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Views;
+
+    class LicitacionesViewModel
+    {
+    }
+}
--- AppIm/AppIm/Views/MenuPage.xaml.cs
+++ AppIm/AppIm/Views/MenuPage.xaml.cs
@@ -15,6 +15,7 @@
         public MenuPage()
         {
             InitializeComponent();
+            
         }
     }
 }
